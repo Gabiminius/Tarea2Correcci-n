efbf82f86dc90d70d2a2d6096434dcd2e3dbfe7c
arenisca <vseron2000@gmail.com>
2024-11-15 18:05:45 -0300

    Modificación: nombre de Excepción

---
+++ tarea2/src/main/java/Logica/DuracionNullException.java
@@ -0,0 +1,12 @@
+/**
+ * Para el caso en que la duración del la reunión sea nula, se tendrá esta clase excepción
+ */
+public class DuracionNullException extends Exception {
+    /**
+     * Constructor
+     * @param message es el mensaje que se mostrará en la excepción
+     */
+    public DuracionNullException(String message) {
+        super(message);
+    }
+}
