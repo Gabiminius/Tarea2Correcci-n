ee520c5c3f1496e684af03cb6271299d99dc2725
arenisca <vseron2000@gmail.com>
2024-11-15 19:35:53 -0300

    Fix: cambio de ubicación carpeta de Exception's

--- tarea2/src/main/java/Exceptions/IniciarReunionIniciadaException.java
+++ tarea2/src/main/java/Exceptions/IniciarReunionIniciadaException.java
@@ -1,12 +1,12 @@
 package Exceptions;
 
-public class IniciarReunionenIniciadaException extends Exception {
+public class IniciarReunionIniciadaException extends Exception {
     /**
      * En caso de que se utilice {@code iniciar()} de Reunión cuando exista
      * una reunión que ya ha comenzado o se intente utilizar el método más de una vez.
      * @param message Mensaje que se mostrará en la excepción.
      */
-    public IniciarReunionenIniciadaException(String message) {
+    public IniciarReunionIniciadaException(String message) {
         super(message);
     }
 }
